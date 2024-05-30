06197ebe4acac755dfd90c2afa4992ad4bf7cd2f
thien00 <thienthcsln@gmail.com>
2024-05-30 14:38:56 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+import request from 'request';
 
 let getHomePage = (req, res) => {
     return res.render('homepage.ejs');
@@ -16,6 +17,17 @@ let postWebhook = (req, res) => {
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            // Get the sendet PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
 
         });
 
@@ -52,7 +64,132 @@ let getWebhook = (req, res) => {
     }
 };
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
 
+    // Checks if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        }
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right picture?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+
+    // Send the response message
+    callSendAPI(sender_psid, response);
+};
+
+async function handlePostback(sender_psid, received_postback) {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    switch (expression) {
+        case 'yes':
+            response = { "text": "Thanks!" }
+            break;
+        case 'no':
+            response = { "text": "Oops, try sending another image." }
+            break;
+        case 'GET_STARTED':
+        case 'RESTART_BOT':
+            await chatbotService.handleStarted(sender_psid);
+            break;
+        case 'MAIN_MENU':
+            await chatbotService.handleSendMainMenu(sender_psid);
+            break;
+
+        case 'LUNCH_MENU':
+            await chatbotService.handleSendLunchMenu(sender_psid);
+            break;
+
+        case 'DINNER_MENU':
+            await chatbotService.handleSendDinnerMenu(sender_psid);
+            break;
+
+        case 'VIEW_APPETIZERS':
+            await chatbotService.handleDetailViewAppetizer(sender_psid);
+            break;
+        case 'VIEW_CHICKEN':
+            await chatbotService.handleDetailViewChicken(sender_psid);
+        case 'VIEW_MEAT':
+            await chatbotService.handleDetailViewMeat(sender_psid);
+            break;
+        case 'VIEW_SEAFOOD':
+            await chatbotService.handleDetailViewSeaFood(sender_psid);
+            break;
+        case 'BACK_TO_MAIN_MENU':
+            await chatbotService.handleBackToMainMenu(sender_psid);
+            break;
+        case 'SHOW_ROOMS':
+            await chatbotService.handleShowDetailRooms(sender_psid);
+            break;
+
+
+        default:
+            response = { "text": `oop! I don't know response with postback ${payload}` }
+    }
+
+    // Send the message to acknowledge the postback
+    //callSendAPI(sender_psid, response);
+}
+
+// Handles response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v20.0/me/messages",
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+};
 
 module.exports = {
     getHomePage: getHomePage,
--- src/server.js
+++ src/server.js
@@ -5,17 +5,16 @@ import webRoutes from "./routes/web";
 
 let app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // config view Engine
 viewEngine(app);
 
 // config web routes
 webRoutes(app);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-let port = process.env.PORT || 8080;
-
-app.listen(port, () => {
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
     console.log("App is running at the port : " + port);
 })
